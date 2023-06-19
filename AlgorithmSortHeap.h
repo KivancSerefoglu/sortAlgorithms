407468573eb2b65c397205f28153f69d62ca18b3
Kıvanç Şerefoğlu <92670408+KivancSerefoglu@users.noreply.github.com>
2023-06-20 00:49:10 +0300

    Add files via upload

---
+++ AlgorithmSortHeap.h
@@ -0,0 +1,16 @@
+#ifndef SORTH
+#define SORTH
+
+#include "SelectionAlgorithm.h"
+#include "BinaryHeap.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+class AlgorithmSortHeap : public SelectionAlgorithm {
+public:
+    AlgorithmSortHeap(int k);
+    int select();
+};
+
+#endif
---
+++ TestBed.h
@@ -0,0 +1,27 @@
+#ifndef TESTBED
+#define TESTBED
+
+#include "SelectionAlgorithm.h"
+#include "AlgorithmSortK.h"
+#include "AlgorithmSortAll.h"
+#include "AlgorithmSortHeap.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+
+class TestBed{
+private:
+    SelectionAlgorithm *algorithm;
+public:
+    void execute();
+    void setAlgorithm (int type, int k);
+    TestBed(int type, int k);
+    ~TestBed();
+};
+
+
+#endif
